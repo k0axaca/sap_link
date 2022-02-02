7e95fa4f5ab6639277a299e6932cb0d701fd88de
kristina <kkraft423@gmail.com>
2022-02-02 11:20:32 -0500

    dispatch added

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,20 @@
 import { createStore, combineReducers } from 'redux';
 import servicesReducer from '../reducers';
 
+const addLoggerToDispatch = (store) => {
+  const dispatch = store.dispatch;
+
+  return action => {  // action is the action object
+    console.group(action.type);
+    console.log('prev state', store.getState());
+    console.log('action', action);
+    const returnValue = dispatch(action);
+    console.log('next state', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  }
+}
+
 const initStore = () => {
 
   const serviceApp = combineReducers({
@@ -8,6 +22,9 @@ const initStore = () => {
   });
     
     const store = createStore(serviceApp);
+
+    store.dispatch = addLoggerToDispatch(store);
+
     return store
   }
 
