68dc6a263a545794ee0ae4f394dd3c0bccc9d515
kristina <kkraft423@gmail.com>
2022-02-07 09:44:49 -0500

    reducer improvements

--- src/reducers/services.js
+++ src/reducers/services.js
@@ -1,16 +1,20 @@
+import { combineReducers } from 'redux';
 import { FETCH_SERVICES_SUCCESS } from '../types';
 
-const INITIAL_STATE = {
-  items: []
-};
+const initServices = () => {
 
-const services = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case FETCH_SERVICES_SUCCESS:
-      return {...state, items: action.services};
-    default:
-      return state;
+  const all = (state = [], action) => {
+    switch(action.type) {
+      case FETCH_SERVICES_SUCCESS:
+        return action.services
+      default:
+        return state
+    }
   }
+
+  return combineReducers({all})
 }
 
-export default services;
+const services = initServices()
+
+export default services
