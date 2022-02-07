c68bb4601812e496d9a0711d0217d831cea10555
kristina <kkraft423@gmail.com>
2022-02-07 16:35:57 -0500

    form validation added

--- src/components/auth/RegisterForm.js
+++ src/components/auth/RegisterForm.js
@@ -5,13 +5,13 @@ const RegisterForm = () => {
   const { register, handleSubmit, watch, formState: {errors} } = useForm();
   const getFormData = data => console.log(data);
 
-  // console.log(watch("fullName")); // watch input value by passing the name of it
+  // console.log(watch("password")); // watch input value by passing the name of it
 
   return (
     <form onSubmit={handleSubmit(getFormData)}>
       <div className="field">
         <div className="control">
-          <input {...register("email")} className="input is-large" type="email" placeholder="Your Email" autoFocus="" autoComplete="email" />
+          <input {...register("email", {required: true}, { pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })} className="input is-large" type="email" placeholder="Your Email" autoFocus="" autoComplete="email" />
           <div className="form-error">
             <span className="help is-danger">Email is required</span>
             <span className="help is-danger">
@@ -23,7 +23,7 @@ const RegisterForm = () => {
       <div className="field">
         <div className="control">
           <input
-            {...register("fullName")}
+            {...register("fullName", { required: true, minLength: 3, maxLength: 20 })}
             className="input is-large"
             type="text"
             placeholder="Full Name"
@@ -38,7 +38,7 @@ const RegisterForm = () => {
       <div className="field">
         <div className="control">
           <input
-            {...register("avatar")}
+            {...register("avatar",  {required: true}, { pattern: /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi})}
             className="input is-large"
             type="text"
             placeholder="Avatar"
@@ -46,14 +46,14 @@ const RegisterForm = () => {
           />
           <div className="form-error">
             <span className="help is-danger">Avatar is required</span>
-            <span className="help is-danger">Avatart is not valid</span>
+            <span className="help is-danger">Avatar is not valid</span>
           </div>
         </div>
       </div>
       <div className="field">
         <div className="control">
           <input
-            {...register("password")}
+            {...register("password", {required: true, minLength: 6})}
             className="input is-large"
             type="password"
             placeholder="Your Password"
@@ -67,7 +67,7 @@ const RegisterForm = () => {
       <div className="field">
         <div className="control">
           <input
-            {...register("email-confirmation")}
+            {...register("password-confirmation", {required: true, minLength: 6})}
             className="input is-large"
             type="password"
             placeholder="Repeat Password"
