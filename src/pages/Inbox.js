64303bf7332ed45b1ed3a1fb1abc72d565b22fbd
G.Mustafa Shaikh <34856975+TheMustafaShaikh@users.noreply.github.com>
2022-02-15 22:56:51 +0500

    done(2)

--- src/pages/Inbox.js
+++ src/pages/Inbox.js
@@ -5,42 +5,68 @@ import { Link ,useParams} from "react-router-dom";
 const Inbox = () =>{
     let {userid,userName} = useParams();
     let [fireStoreMessages,setFirestoreMessages] = React.useState([]);
+    let [userIdArray, setuserIdArray] = React.useState([])
     const user = {
         fullName:userName,
         uid:userid
     };
+    const getid = async (id) =>{
+        var res = await db.collection("profiles").where("uid","==",id).get();
+        return res.docs[0].data();
+      }
+  
 
     useEffect(()=>{
         db.collection("messages").where("users","array-contains",userid).onSnapshot((res)=>{
+
             setFirestoreMessages(res.docs)
+            var associateArray = []
+            res.docs.map((el,i)=>{
+                var senderid = el.data().users.filter((e)=>e != userid);
+                 getid(senderid[0]).then((re)=>{
+                     associateArray.push(re)
+                      if(i == res.docs.length-1){
+                        setuserIdArray(associateArray);
+                      }
+                    });
+               
+            })
+           
           })
     },[])
 
+  
+ 
 
-    const _renderLastMesssage = (message,user,detailsOfSender) =>{
+   
+
+    const _renderLastMesssage =  (message,user,detailsOfSender) =>{
         const lastMessage = message[message.length-1];
-        const [name,msg] = lastMessage.split(":");
-        console.log("CHAT",user,name,detailsOfSender)
-        const SenderId = detailsOfSender.users.filter((e)=> e != user.uid);
-        console.log("CHAT",SenderId)
+        var [name,msg] = lastMessage.split(":");
         
-        return (
-          <Link to={`/message/${user.fullName}/${user.uid}/${SenderId[0]}`}>
-              <h1 style={{fontWeight:"bold"}}>{name}</h1>
-              <p>{msg}</p>
-          </Link>
-        )
+        const SenderId = detailsOfSender.users.filter((e)=> e != user.uid);
+        const fullNames = userIdArray.filter((el)=>el.uid == SenderId)
+    
+        
+        
+            return (
+              <Link to={`/message/${user.fullName}/${user.uid}/${SenderId[0]}`}>
+                  <h1 style={{fontWeight:"bold"}}>{fullNames[0].fullName}</h1>
+                  <p>{msg}</p>
+              </Link>
+            )
+        
         //Kristina: Hi!
       }
 
    
-  if(fireStoreMessages.length == 0){
+  if(fireStoreMessages.length == 0 && userIdArray.length == 0){
     return <div className="navbar-item">No Messages :(</div>
   }else{
   return (
       <div>
         {
-          fireStoreMessages?.map((ele)=>_renderLastMesssage(ele.data().chat,user,ele.data()))
+          userIdArray.length > 0 && fireStoreMessages?.map((ele)=>_renderLastMesssage(ele.data().chat,user,ele.data()))
         }
       </div>
     )
